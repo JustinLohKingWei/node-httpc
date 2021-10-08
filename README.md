d52e58968e0de5c16790cb59da442873c55ef4c1
Justin Loh <justinlohkingwei@gmail.com>
2021-10-08 15:24:06 -0400

    Updated readme and added header options (in strings)

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # node-httpc
-Node CLI tool made to simulate curl like functions
+Node CLI tool made to simulate curl like functions.Httpc currently only supports HTTP GET and POST requests only
 
-#Usage
-This tool supports only GET requests for now , with POST features currently in development
+# Installaton
+1) Clone the repository. Make sure that node is installed on your machine.
+2) Depending on the branch, the folder "node_modules" may or may not exist. If it does not exist, open a terminal in the folder, and use the command "npm install" to install node modules.
+3) try "httpc help" to check if the application was properly installed
 
+# Example Get Request (non verbose)
+httpc get 'http://httpbin.org/get?course=networking&assignment=1'
+
+# Example Get Request (verbose)
+httpc get -v 'http://httpbin.org/get?course=networking&assignment=1'
+
+
+# Post Request
+httpc post -h "Content-Type: application/json" --d '{"Assignment": 1}' http://httpbin.org/post
 
 
--- httpc.js
+++ httpc.js
@@ -83,7 +83,7 @@ yargs
       console.log(argv.d);
       const client = net.connect(80, myUrl.host, function () {
         client.write(
-          `POST ${myUrl.path} HTTP/1.1\n` + `Host: ${myUrl.host}\n` + `Content-Type: application/json;\n` + `Content-Length: ${argv.d.length}` + `\r\n\r\n${argv.d}\n\n`
+          `POST ${myUrl.path} HTTP/1.1\n` + `Host: ${myUrl.host}\n` + `${argv.h};\n` + `Content-Length: ${argv.d.length}` + `\r\n\r\n${argv.d}\n\n`
         );
         console.log("Connected to server!");
       });
@@ -143,29 +143,3 @@ yargs
   });
 
 yargs.argv;
-
-//https://thecodebarbarian.com/building-a-cli-tool-with-node-js.html
-//https://blog.bitsrc.io/how-to-build-a-command-line-cli-tool-in-nodejs-b8072b291f81
-//https://alanstorm.com/yargs-and-command-line-argument-processing-in-nodejs/
-//https://nodejs.org/en/knowledge/command-line/how-to-parse-command-line-arguments/
-
-// new Promise((resolve, reject) => {
-//   let buff = Buffer.of();
-//   client.on("data", function (data) {
-//     console.log("Received " + data.length + " bytes\n" + data);
-//     buff = Buffer.concat(buff, data);
-//   });
-//   client.on("end", function () {
-//     console.log("disconnected from server");
-//     //client.end();
-//     resolve(buff.toString());
-//   });
-//   client.on("error", function (e) {
-//     console.log("Connection error");
-//     // client.end();
-//     reject(e);
-//   });
-// })
-//   .then((data) => console.log(`Finished recieving data: ${data}`))
-//   .catch((e) => console.log(`An error occured: ${e}`));
-//`GET /get?${myUrl.query}} HTTP/1.1\n` + `Host: ${myUrl.host}\n\n`
